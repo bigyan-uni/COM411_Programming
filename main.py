dec1984b5ae25966527aed8f29eccb8d00b57599
Bigyan Acharya <2achab31@solent.ac.uk>
2023-09-28 12:36:46 +0100

    Hello World!

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Hello World!")
+name = "Bigyan"
+age = 19
+print(f"My name is {name} and I am {age} years old.")
+
+change = True
+
+#type in console
+#git status
+#git add .
+#git commit -m "Hello World!"
+#git push
