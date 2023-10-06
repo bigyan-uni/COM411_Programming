62f5012a400400e32f5cd8fb356170f1ccfa917c
Bigyan Acharya <2achab31@solent.ac.uk>
2023-10-06 15:35:54 +0100

    Added code to demonstrate the use of a logical and operator.

--- Week2/multiple_conditions.py
+++ Week2/multiple_conditions.py
@@ -1,3 +1,4 @@
+#  Logical OR Operator
 adventure_type = input('What type of adventure should I have?')
 
 if adventure_type == 'scary' or adventure_type == 'short':
@@ -6,3 +7,14 @@ elif adventure_type == 'safe' or adventure_type == 'long':
     print('Taking the safe route!')
 else:
     print("Not sure which route to take.")
+
+#  Logical AND Operator
+user_listen = input("What did I hear?")
+user_sight = input("What did I see?")
+
+if user_listen == 'grr' and user_sight == 'two red eyes':
+    print("There is a scary creature. I should get out of here!")
+elif user_listen == 'hoot' and user_sight == 'some big eyes':
+    print("It's just an owl.")
+else:
+    print("I am a little scared but I will continue.")
