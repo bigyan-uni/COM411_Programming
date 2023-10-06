3f7395c64795d6a0ee7c3f7e070fc029de56da25
Bigyan Acharya <2achab31@solent.ac.uk>
2023-10-06 14:22:44 +0100

    Added code to demonstrate the use of an if statement.

---
+++ Week2/if_statements.py
@@ -0,0 +1,7 @@
+book_genre = input("What type of book is this?")  # stores the genre of book
+
+if book_genre == "adventure":
+    print(f'I like {book_genre} books!')  # formatted string prints appropriate message
+
+
+print("Finished reading book.")
