38e501f8ad42d250aac8de190efe7cdd7a873aa4
Bigyan Acharya <2achab31@solent.ac.uk>
2023-10-06 14:42:02 +0100

    Added code to demonstrate the use of an if...elif...else statement.

--- Week2/if_statements.py
+++ Week2/if_statements.py
@@ -1,4 +1,4 @@
-#First Program
+#  First Program
 book_genre = input("What type of book is this?")  # stores the genre of book
 
 if book_genre == "adventure":
@@ -7,14 +7,32 @@ if book_genre == "adventure":
 
 print("Finished reading book.")
 
-#Second Program
+#  Second Program
 activity_type = input("Please enter the activity to be performed.")
-#stores user's input
+#  stores user's input
 
 if activity_type == "calculate":
     print("Performing calculations...")
-    #prints appropriate message
+    #  prints appropriate message
 else:
     print("Performing activity...")
-    #prints alternative message
+    #  prints alternative message
 print('Activity completed!')
+
+#  Third Program
+user_direction = input('Towards which direction should I go (up, down, left or right)?')
+#  stores direction user wishes to move towards
+
+if user_direction == 'up':
+    print('I am moving in an upward direction!')
+elif user_direction == 'down':
+    print('I am moving in a downward direction!')
+elif user_direction == 'left':
+    print('I am moving towards the left!')
+elif user_direction == 'right':
+    print('I am moving towards the right!')
+else:
+    print('That is not a valid direction.')
+#  fallback message in case user inputs invalid direction
+
+
