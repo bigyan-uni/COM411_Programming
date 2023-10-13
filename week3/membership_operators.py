0bc76b5c07e86e80c4cc8d068c59dc68e4ce11f2
Bigyan Acharya <2achab31@solent.ac.uk>
2023-10-13 15:31:00 +0100

    Added code to demonstrate the use of membership operators with a for loop.

---
+++ week3/membership_operators.py
@@ -0,0 +1,10 @@
+#  initialising variable
+user_phrase = input('What phrase do you see?')
+
+print('outputting...')
+print('The phrase is: ')
+
+#  for loop
+for char in user_phrase:
+    print(char)
+
