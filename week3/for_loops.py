8b9a41d8ac46bdadfe019564397af081d5c3112c
Bigyan Acharya <2achab31@solent.ac.uk>
2023-10-13 14:53:37 +0100

    Added code to demonstrate the use of a simple for loop.

---
+++ week3/for_loops.py
@@ -0,0 +1,15 @@
+#  initiating variables
+mount_num = int(input('How many mountains should I display?'))
+print('Displaying...')
+
+#  for loop
+for i in range(0,mount_num):
+    print('           __ ')
+    print('          /  \_  ')
+    print('         /^    \ ')
+    print('        /  ^    \_ ')
+    print('      _/ ^ ^     ^\ ')
+    print('     /  ^     ^    \ ')
+
+#  final message
+print('Done!')
