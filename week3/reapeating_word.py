d9d1f979361b2d9cb8f5a26407f78d75d558853a
Bigyan Acharya <2achab31@solent.ac.uk>
2023-10-13 14:33:55 +0100

    Added code to demonstrate the use of a while loop with len function.

---
+++ week3/reapeating_word.py
@@ -0,0 +1,9 @@
+#  getting user's input
+user_phrase = input('Please enter a phrase:')
+
+#  creating while loop
+count = 0
+
+while count != len(user_phrase):
+    print('Hi', end='')
+    count += 1
