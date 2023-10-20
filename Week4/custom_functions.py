9418fbfb2922e882327af976835624d3c3a14670
Bigyan Acharya <2achab31@solent.ac.uk>
2023-10-20 14:39:16 +0100

    Added code to demonstrate the use of a user-defined function with a nested decision.

--- Week4/custom_functions.py
+++ Week4/custom_functions.py
@@ -1,8 +1,20 @@
-#  initialising function
+#  initialising listen function
 def listen():
     sound = input('What sound did you hear?')
     print(f'That was a loud {sound}!')
 
 
-#  calling function
+#  calling listen function
 listen()
+
+
+#  initialising identify function
+def identify():
+    user_observation = input('What lies before us?')
+    if user_observation == 'a large boulder':
+        print("It's time to run!")
+    else:
+        print('We will be fine.')
+
+#  calling identify function
+identify()
