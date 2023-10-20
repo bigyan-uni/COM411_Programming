8d1ae57ebf896cc7b31f5099d597ba2079c19917
Bigyan Acharya <2achab31@solent.ac.uk>
2023-10-20 14:55:22 +0100

    Added code to demonstrate the use of a user-defined function with a loop.

--- Week4/custom_functions.py
+++ Week4/custom_functions.py
@@ -37,3 +37,18 @@ def escape_by(plan):
 escape_by("jumping over")
 escape_by("running around")
 escape_by("cross bridge ahead")
+
+
+#  creating function to help the player cross the bridge
+def cross_bridge(steps):
+    for i in range(steps):
+        print("Crossed step.")
+    if steps > 5:
+        print("The bridge is collapsing!")
+    else:
+        print("We must keep going!")
+
+
+#  calling function with example arguments
+cross_bridge(3)
+cross_bridge(6)
