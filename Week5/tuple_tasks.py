165d1f6b25396a5ea5c87d2227e4beaf378eccbf
Bigyan Acharya <2achab31@solent.ac.uk>
2023-10-27 15:38:19 +0100

    Added code to demonstrate how to nest tuples and lists.

--- Week5/tuple_tasks.py
+++ Week5/tuple_tasks.py
@@ -30,3 +30,30 @@ def run_task2():
 
 
 run_task2()
+
+#  variation to demonstrate how to nest tuples and lists.
+
+
+def steps():
+    likelihoods = [("step 1", 50), ("step 2", 38), ("step 3", 27), ("step 4", 99), ("step 5", 4)]
+    return likelihoods
+
+
+def run_task3():
+    good_steps = []
+    bad_steps = []
+    step_likelihood = steps()
+    for i in range(len(step_likelihood)):
+        tup = step_likelihood[i]
+        for j in range(len(step_likelihood[i])):
+            if j == 1:
+                if tup[j] >= 50:
+                    bad_steps.append(tup[0])
+                else:
+                    good_steps.append(tup[0])
+    good = len(good_steps)
+    bad = len(bad_steps)
+    print(f"Good steps: {good}, Bad steps: {bad}")
+
+
+run_task3()
