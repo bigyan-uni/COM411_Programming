03ebd7a362afd1cb9b6d55e6904f58252ed7646a
Bigyan Acharya <2achab31@solent.ac.uk>
2023-10-27 14:22:21 +0100

    Added code to demonstrate a simple list.

---
+++ Week5/list_tasks.py
@@ -0,0 +1,11 @@
+def directions():
+    steps = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+    return steps
+
+
+def run_task1():
+    print(directions())
+
+
+if __name__ == "__main__":
+    run_task1()
