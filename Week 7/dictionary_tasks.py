e91c9ef7729b0d96e0c9ebe8a6de727f4045a471
Bigyan Acharya <2achab31@solent.ac.uk>
2023-11-10 15:39:42 +0000

    Added code to demonstrate a simple dictionary.

---
+++ Week 7/dictionary_tasks.py
@@ -0,0 +1,11 @@
+def pattern():
+    sequences = {"Short Sequence": 3, "Medium Sequence": 2}
+    sequences["Long Sequences"] = 1
+    return sequences
+
+
+def run():
+    print(pattern())
+
+
+run()
