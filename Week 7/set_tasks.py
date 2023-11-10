f6c6110efa644087e2156d8c1eb74cf45d2bab6d
Bigyan Acharya <2achab31@solent.ac.uk>
2023-11-10 14:27:42 +0000

    Added code to demonstrate a simple set.

---
+++ Week 7/set_tasks.py
@@ -0,0 +1,11 @@
+def observed():
+    observations = set()
+    observations = {"Car", "Sky Scraper", "Sky Scraper", "Bike", "House", "House"}
+    return observations
+
+
+def run_task1():
+    print(observed())
+
+
+run_task1()
