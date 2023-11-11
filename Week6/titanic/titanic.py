81466a00770c092ad8ba8c1fa0a8c539f0b04f06
Bigyan Acharya <2achab31@solent.ac.uk>
2023-11-11 12:48:45 +0000

    Added code to display a menu of options.

--- Week6/titanic/titanic.py
+++ Week6/titanic/titanic.py
@@ -4,6 +4,7 @@ records = []
 headings = []
 
 
+#  imports data set and separates header from data
 def load_data(file_path):
     print("Loading data...")
     with open(file_path) as file:
@@ -18,6 +19,21 @@ def run():
     load_data("titanic.csv")
     num_records = len(records)
     print(f"Successfully loaded {num_records} records.")
+    selected_option = display_menu()
+    print(f"You have selected option: [{selected_option}]")
 
 
+#  implementing a menu to process data
+def display_menu():
+    print("""
+    Please select one of the following options:
+    [1] Display the names of all passengers
+    [2] Display the number of passengers that survived
+    [3] Display the number of passengers per gender
+    [4] Display the number of passengers per age group
+    [5] Display the number of survivors per age group"
+    """)
+    return int(input())
 
+
+run()
