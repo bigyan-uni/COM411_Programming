f976ba60459a92fe225c648f7cafe43aca632727
Bigyan Acharya <2achab31@solent.ac.uk>
2023-11-11 11:23:58 +0000

    Added code to extract data from the CSV file.

--- Week6/csv_tasks.py
+++ Week6/csv_tasks.py
@@ -17,3 +17,23 @@ def run_task1():
 
 if __name__ == "__main__":
     run_task1()
+
+
+#  Task 2 for CSV files
+def extract(file_path):
+    print("Extracting...")
+    with open(file_path) as file:
+        contents = csv.reader(file)
+        headings = next(contents)
+        names = ""
+        for values in contents:
+            names += f"{values}\n"
+    print("Done!\nThe extracted items are as follows:")
+    print(names)
+
+
+def run_task2():
+    extract("clothing.csv")
+
+
+run_task2()
