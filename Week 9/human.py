cdb411a8ab1bc3206290ddfdb5dd070e1908f523
Bigyan Acharya <2achab31@solent.ac.uk>
2023-11-24 15:31:19 +0000

    Updated classes human and robot with instance methods.

--- Week 9/human.py
+++ Week 9/human.py
@@ -16,9 +16,38 @@ class Human:
     def __str__(self):
         return f"Human {self.name} is {self.age} years old."
 
+    def grow(self):
+        self.age += 1
+
+    def eat(self, amount):
+        new_energy = self.energy + amount
+        if new_energy > Human.MAX_ENERGY:
+            self.energy = Human.MAX_ENERGY
+        else:
+            self.energy = new_energy
+
+    def move(self, distance):
+        distance = int(distance)
+        new_energy = self.energy - distance
+        if new_energy < 0:
+            self.energy = 0
+        else:
+            self.energy = new_energy
+
 
 if __name__ == "__main__":
     human = Human()
     human.display()
     print(human)
     print(repr(human))
+    human.grow()
+    print(human.age)
+    human.energy = 0
+    human.eat(10)
+    print(human.energy)
+    human.eat(115)
+    print(human.energy)
+    human.move(15)
+    print(human.energy)
+    human.move(90)
+    print(human.energy)
--- Week 9/robot.py
+++ Week 9/robot.py
@@ -16,9 +16,38 @@ class Robot:
     def __str__(self):
         return f"Robot {self.name} is {self.age} years old."
 
+    def grow(self):
+        self.age += 1
+
+    def eat(self, amount):
+        new_energy = self.energy + amount
+        if new_energy > Robot.MAX_ENERGY:
+            self.energy = Robot.MAX_ENERGY
+        else:
+            self.energy = new_energy
+
+    def move(self, distance):
+        distance = int(distance)
+        new_energy = self.energy - distance
+        if new_energy < 0:
+            self.energy = 0
+        else:
+            self.energy = new_energy
+
 
 if __name__ == "__main__":
     robot = Robot()
     robot.display()
     print(robot)
     print(repr(robot))
+    robot.grow()
+    print(robot.age)
+    robot.energy = 0
+    robot.eat(10)
+    print(robot.energy)
+    robot.eat(115)
+    print(robot.energy)
+    robot.move(15)
+    print(robot.energy)
+    robot.move(90)
+    print(robot.energy)
