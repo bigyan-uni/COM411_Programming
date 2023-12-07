2e1b3fadcb6c571d7100da83fbf3a111a25846e4
Bigyan Acharya <2achab31@solent.ac.uk>
2023-12-07 13:54:28 +0000

    Added code to export JSON data to a file.

--- Week 7/json_tasks.py
+++ Week 7/json_tasks.py
@@ -21,5 +21,25 @@ def run():
     read("futurama.json")
 
 
+def read_task2(file_path):
+    print("Reading...", end="")
+    with open(file_path) as file:
+        data = json.load(file)
+    print("Done!")
+    return data
+
+
+def save(file_path, data):
+    print("Exporting...", end="")
+    with open(file_path, "w") as file:
+        json.dump(data, file, indent=4)
+    print("Done!")
+
+
+def run_task2():
+    data = read_task2("futurama.json")
+    save("exported.json", data)
+
+
 if __name__ == "__main__":
-    run()
+    run_task2()
